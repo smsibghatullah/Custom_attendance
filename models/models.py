8bc5a266571058de9c01d0b85ec39962732188f5
mubeen1020 <m.mubeen1020@gmail.com>
2024-08-30 15:11:48 +0500

    working

--- models/models.py
+++ models/models.py
@@ -16,8 +16,8 @@ class Attendance(models.Model):
             last_attendance = self.env['hr.attendance'].search([('employee_id', '=', employee.id),('check_in', '<=', current_time)], order='id desc',
                                                                limit=1)
             if last_attendance:
-                last_date = last_attendance.check_in.strftime('%Y-%m-%d')
-                if current_date != last_date:
+                # last_date = last_attendance.check_in.strftime('%Y-%m-%d')
+                # if current_date != last_date:
                     self.env['hr.attendance'].create({
                         'employee_id': employee.id,
                         'check_in': current_time,
@@ -49,3 +49,27 @@ class Attendance(models.Model):
                             'empl_name': employee.name,
                             'datetime': format_datetime(self.env, current_time, dt_format=False),
                         })
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+
+    @api.constrains('payment_method_line_id')
+    def _check_payment_method_line_id(self):
+        ''' Ensure the 'payment_method_line_id' field is not null.
+        Can't be done using the regular 'required=True' because the field is a computed editable stored one.
+        '''
+        for pay in self:
+            print(pay.journal_id,"pppppppppppppppppppppppppp",pay.payment_method_line_id.journal_id,"=================================",pay.payment_method_line_id,"oooooooooooooo",pay.payment_method_line_id.name)
+
+            if not pay.payment_method_line_id:
+                raise ValidationError(_("Please define a payment method line on your payment."))
+            elif pay.payment_method_line_id.journal_id and pay.payment_method_line_id.journal_id != pay.journal_id:
+                payment_method_line = self.env['account.payment.method.line'].search([
+                ('journal_id', '=', pay.journal_id.id),
+                ('payment_method_id', '=', pay.payment_method_line_id.payment_method_id.id)
+                ], limit=1)
+                print(payment_method_line,"mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm")
+                if payment_method_line:
+                    pay.payment_method_line_id = payment_method_line
+                else:
+                    raise ValidationError(_("The selected payment method is not available for this payment, please select the payment method again."))
